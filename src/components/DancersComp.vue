7b1320eb4e841375adf7e9d2df659cca05cf01c7
helena-hollander <helena.hollander@gmail.com>
2024-06-13 18:12:01 +0200

    lol med scrolltrigger

--- src/components/DancersComp.vue
+++ src/components/DancersComp.vue
@@ -8,11 +8,19 @@ onMounted(()=>{
 let tl = gsap.timeline({
 scrollTrigger:{
   trigger: '#dancerstitle',
-  start: '-50% center',
+  start: '-100% center',
   end: 'bottom center',
   scrub: true,
   markers: true
-}
+},
+
+// scrollTrigger:{
+//   trigger: '#dancersanidown',
+//   start: 'top center',
+//   end: '200% center',
+//   scrub: true,
+//   markers: true
+// }
 
 });
  
@@ -20,8 +28,8 @@ tl.from("#dancerstitle", {
   ease: "power.out",
   x: 2000,
 })
-// .to("#dancerstitle", {
-//   y: 400,
+// .to("#dancersanidown", {
+//   y: 800,
  
 //     });
 
@@ -35,7 +43,9 @@ tl.from("#dancerstitle", {
 
 <template>
 <div class="translate-x-[700px]">
- <h2 class="font-display text-white text-9xl blur-sm translate-y-[100px]" id="dancerstitle">dancers</h2>  
+  <div id="dancersanidown">
+ <h2 class="font-display text-white text-9xl blur-sm translate-y-[100px]" id="dancerstitle">dancers</h2> 
+  </div> 
  <p class="font-brøditalic text-white text-3xl translate-y-[100px] translate-x-[-200px]">Afra Hosseini Kaladjahi<br>Ama Kyei<br>Anastasija Olescuka<br>Andreas Sanchez<br>Anna Fitoussi<br>Camille Prieux<br>Chiara Gilioli<br>Eleanor Campbell<br>Freddy Houndekindo<br>Gloria Kapako<br>Ida Holmlund<br>Katie Jacobson<br>Lilian Steiner<br>Mohamed Y. Shika<br>Noam Segal<br>Omar Velasquez Rojas<br>Rebecca Livaniou<br>Robin Johansson<br>Shai Faran</p>
 </div>
 
--- src/components/TourComp.vue
+++ src/components/TourComp.vue
@@ -4,31 +4,77 @@ import { ScrollTrigger } from 'gsap/all';
 import { onMounted } from 'vue';
 gsap.registerPlugin(ScrollTrigger)
 
+onMounted(()=>{
+let tl = gsap.timeline({
+scrollTrigger:{
+  trigger: '#tourtitle',
+  start: '-170% center',
+  end: 'top center',
+  scrub: true,
+  markers: true
+}
+
+});
+ 
+tl.from("#tourtitle", {
+  ease: "power.out",
+  x: -2000,
+})
+
+// tl.to("#toursectionanidown", {
+//   y: 100,
+
+// })
+
+
+   
+ })
+
+
 
 </script>
 
 <template>
-<h2 class="font-bold text-white text-10xl translate-y-[400px] translate-x-[1000px] blur-sm">tour dates</h2>
-<div id="dates" class="grid w-[250vw] grid-cols-8 gap-x-[80px]">
-<div id="28.6" class="text-white translate-y-[400px]">
+  <div id="toursectionanidown">
+<h2 id="tourtitle" class="font-bold text-white text-10xl translate-y-[300px] translate-x-[1000px] blur-sm">tour dates</h2>
+<div id="dates" class="grid w-[220vw] grid-cols-8 gap-x-[80px] m-20 translate-y-[270px]">
+
+<div id="30.10" class="text-white">
+  <p class="font-display text-7xl">30.10.24</p>
+  <p class="font-display text-4xl translate-y-[-78px] translate-x-[390px]"> / 19:30</p>
+  <p class="font-brøditalic text-4xl translate-y-[-64px]">Stora Teatern, Gothenburg</p>
+</div>
+<div id="23.8" class="text-white">
+  <p class="font-display text-7xl">23.8.24</p>
+  <p class="font-display text-4xl translate-y-[-78px] translate-x-[336px]"> / 19:30</p>
+  <p class="font-brøditalic text-4xl translate-y-[-64px]">Vitabergsparken, Stockholm</p>
+</div>
+<div id="29.6" class="text-white">
+  <p class="font-display text-7xl">29.6.24</p>
+  <p class="font-display text-4xl translate-y-[-78px] translate-x-[336px]"> / 19:30</p>
+  <p class="font-brøditalic text-4xl translate-y-[-64px]">Blågårds Plads, Copenhagen</p>
+</div>
+<div id="28.6" class="text-white">
   <p class="font-display text-7xl">28.6.24</p>
   <p class="font-display text-4xl translate-y-[-78px] translate-x-[336px]"> / 19:30</p>
   <p class="font-brøditalic text-4xl translate-y-[-64px]">Blågårds Plads, Copenhagen</p>
 </div>
-<div id="27.6" class="text-white translate-y-[400px]">
+<div id="27.6" class="text-white">
   <p class="font-display text-7xl">27.6.24</p>
   <p class="font-display text-4xl translate-y-[-78px] translate-x-[336px]"> / 19:30</p>
   <p class="font-brøditalic text-4xl translate-y-[-64px]">Blågårds Plads, Copenhagen</p>
 </div>
-<div id="26.6" class="text-white translate-y-[400px]">
+<div id="26.6" class="text-white">
   <p class="font-display text-7xl">26.6.24</p>
   <p class="font-display text-4xl translate-y-[-78px] translate-x-[336px]"> / 19:30</p>
   <p class="font-brøditalic text-4xl translate-y-[-64px]">Blågårds Plads, Copenhagen</p>
 </div>
-<div id="25.6" class="text-white translate-y-[400px]">
+<div id="25.6" class="text-white">
   <p class="font-display text-7xl">25.6.24</p>
   <p class="font-display text-4xl translate-y-[-78px] translate-x-[336px]"> / 19:30</p>
   <p class="font-brøditalic text-4xl translate-y-[-64px]">Blågårds Plads, Copenhagen</p>
 </div>
+</div>
+
 </div>
 </template>
